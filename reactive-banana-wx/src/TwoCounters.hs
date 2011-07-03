e285b51bb69c95d5d1565976041db99bba4cc1d9
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2011-07-03 20:31:54 +0200

    Add new example with two counters.

---
+++ reactive-banana-wx/src/TwoCounters.hs
@@ -0,0 +1,50 @@
+{-----------------------------------------------------------------------------
+    reactive-banana-wx
+    
+    Example: Two Counters.
+------------------------------------------------------------------------------}
+module Main where
+
+import Control.Monad
+import Graphics.UI.WX hiding (Event)
+import Reactive.Banana
+import Reactive.Banana.WX
+
+{-----------------------------------------------------------------------------
+    Main
+------------------------------------------------------------------------------}
+main = start $ do
+    f       <- frame [text := "Two Counters"]
+    bup     <- button f [text := "Up"]
+    bdown   <- button f [text := "Down"]
+    bswitch <- button f [text := "Switch Counters"]
+    out1    <- staticText f []
+    out2    <- staticText f []
+    
+    set f [layout := margin 10 $
+            column 5 [row 5 [widget bup, widget bdown, widget bswitch],
+                      row 30 [widget out1, widget out2]]]
+    
+    network <- compile $ do
+        eup     <- event0 bup   command
+        edown   <- event0 bdown command
+        eswitch <- event0 bswitch command
+        
+        let
+            -- do we act on the left button?
+            firstcounter :: Behavior Bool
+            firstcounter = accumB True $ not <$ eswitch
+        
+            -- joined state of the two counters
+            counters :: Event (Int, Int)
+            counters = accumE (0,0) $
+                union ((increment <$> firstcounter) `apply` eup)
+                      ((decrement <$> firstcounter) `apply` edown)
+            
+            increment left _ (x,y) = if left then (x+1,y) else (x,y+1)
+            decrement left _ (x,y) = if left then (x-1,y) else (x,y-1)
+    
+        sink out1 [text :== ("0", show . fst <$> counters)]
+        sink out2 [text :== ("0", show . snd <$> counters)]
+    
+    run network
