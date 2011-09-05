de8dd7a6e8ecdbe1ce1fa3bc0ef4db4e16ce926b
localuser <localuser@MusTechMac-W87303XHX85.local>
2011-09-05 16:37:11 +0100

    Rename 'Discrete' to 'DiscreteD'

--- reactive-banana/src/Reactive/Banana.hs
+++ reactive-banana/src/Reactive/Banana.hs
@@ -12,7 +12,7 @@ module Reactive.Banana (
     Event, Behavior, Discrete,
     ) where
 
-import Reactive.Banana.Incremental hiding (Discrete)
+import Reactive.Banana.Incremental hiding (DiscreteD)
 import qualified Reactive.Banana.Incremental as Polymorph
 import Reactive.Banana.Model hiding (interpret, Event, Behavior)
 import qualified Reactive.Banana.Model as Polymorph
@@ -20,4 +20,4 @@ import Reactive.Banana.Implementation
 
 type Event    = Polymorph.Event PushIO
 type Behavior = Polymorph.Behavior PushIO
-type Discrete = Polymorph.Discrete PushIO
+type Discrete = Polymorph.DiscreteD PushIO
