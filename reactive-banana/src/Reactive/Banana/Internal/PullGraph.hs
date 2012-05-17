1101250eeb092bdf0d8ddd43d30e26dcc2767655
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2012-05-17 12:54:17 +0200

    Uniform nodes for Event and Behavior. Implement initialization procedure.

--- reactive-banana/src/Reactive/Banana/Internal/PullGraph.hs
+++ reactive-banana/src/Reactive/Banana/Internal/PullGraph.hs
@@ -18,16 +18,20 @@ module Reactive.Banana.Internal.PullGraph (
     ) where
 
 import Control.Applicative
-import Control.Monad (join, liftM, ap)
+import Control.Monad (join, liftM, ap, when)
 
 import Data.Maybe (fromJust)
+import Data.Functor.Identity
+
+import qualified Data.HashSet as Set
+import Data.Unique.Really
 import qualified Data.Vault as Vault
 
 import System.IO.Unsafe (unsafePerformIO)
 
-import Data.Unique.Really
-
 import Reactive.Banana.Internal.InputOutput
+
+type Set = Set.HashSet
 
 {-----------------------------------------------------------------------------
     Event graph and network
@@ -42,17 +46,17 @@ data Graph a
     , grOutput  :: Event a      -- output event
     }
 
--- pointer to a single node of type  a  in the graph
-data Node a
-    = Node
-    { nodeValue  :: !(Vault.Key (Maybe a))   -- currently calculated value
-    , nodeAccum  :: !(Vault.Key a)           -- value to be accumulated
-    , nodeName   :: !Unique                  -- unique identifier
+-- keys associated to a single node of type  a  in the graph
+data Keys a
+    = Keys
+    { keyValue  :: !(Vault.Key (Maybe a))   -- currently calculated value
+    , keyAccum  :: !(Vault.Key a)           -- value to be accumulated
+    , keyName   :: !Unique                  -- unique identifier
     }
 
 -- create new keys
-newNode :: IO (Node a)
-newNode = Node <$> Vault.newKey <*> Vault.newKey
+newKeys :: IO (Keys a)
+newKeys = Keys <$> Vault.newKey <*> Vault.newKey <*> newUnique
 
 -- network monad for modifying the event network and graph
 newtype Network a = Network { runNetwork :: forall b.
@@ -62,7 +66,7 @@ newtype Network a = Network { runNetwork :: forall b.
     }
 
 instance Functor Network where fmap = liftM
-instance Applicative Network where (<*>) = ap
+instance Applicative Network where pure = return; (<*>) = ap
 
 instance Monad Network where
     return a = Network $ \s -> (a, s, id)
@@ -73,38 +77,45 @@ instance Monad Network where
 
 -- Perform a calculation with side effects and cache the result.
 -- If the cached value is already available, we don't perform the side effect.
-cached :: Node a -> Network (Maybe a) -> Network (Maybe a)
+cached :: Keys a -> Network (Maybe a) -> Network (Maybe a)
 cached self m = Network $ \graph ->
-    case Vault.lookup (nodeValue self) (grValues graph) of
+    case Vault.lookup (keyValue self) (grValues graph) of
         Just a  -> (a,graph,id)   -- lookup succeeded
         Nothing ->                -- lookup failed
             let (a, graph2, f) = runNetwork m graph
-            in  (a, graph2 { grAccums = Vault.insert (nodeValue self) a
+            in  (a, graph2 { grAccums = Vault.insert (keyValue self) a
                                                      (grValues graph2) }, f) 
 
 -- read the currently accumulated value
-readAccum :: Node a -> Network a
+readAccum :: Keys a -> Network a
 readAccum self = Network $
-    \graph -> case Vault.lookup (nodeAccum self) (grAccums graph) of
+    \graph -> case Vault.lookup (keyAccum self) (grAccums graph) of
         Just x -> (x, graph, id)
 
 -- schedule a write of the new accumulated value
-writeAccum :: Node a -> a -> Network ()
-writeAccum self x = Network $
-    \graph -> ((), graph,
-        \g -> g { grAccums = Vault.insert (nodeAccum self) x (grAccums g) })
+writeAccum :: Keys a -> a -> Network ()
+writeAccum self x = Network $ \graph ->
+    ( (), graph
+    , \g -> g { grAccums = Vault.insert (keyAccum self) x (grAccums g) }
+    )
 
--- test whether we have initialized the accumulator
-isAccumInitialized :: Node a -> Network Bool
-isAccumInitialized self = Network $
-    \graph -> (isJust (Vault.lookup (nodeAccum self) (grAccums graph), graph, id)
+-- test whether we have initialized the node
+readIsInit :: Keys a -> Network Bool
+readIsInit self = Network $
+    \graph -> (Set.member (keyName self) (grIsInit graph), graph, id)
 
+-- set the initialization field
+writeIsInit :: Keys a -> Network ()
+writeIsInit self = Network $ \graph ->
+        ( ()
+        , graph { grIsInit = Set.insert (keyName self) (grIsInit graph) }
+        , id )
 
 -- make sure that the node is being evaluated
 -- because it may accumulate state that we don't want to miss
 -- TODO: Undo this when the Haskell RTS garbage collects this node!
-demand :: SomeNode -> Network ()
-demand somenode = Network $ \graph ->
+addDemand :: SomeNode -> Network ()
+addDemand somenode = Network $ \graph ->
     ((), graph, \g -> g { grDemand = somenode : grDemand g })
 
 {-----------------------------------------------------------------------------
@@ -123,71 +134,78 @@ compileToAutomaton :: Event b -> Automaton b
 compileToAutomaton e = fromStateful step $
     Graph { grValues = Vault.empty
           , grAccums = Vault.empty
+          , grIsInit = Set.empty
           , grDemand = [E e]
           , grOutput = e }
 
 {-----------------------------------------------------------------------------
-    Building events with
+    Building events and behaviors with
     observable sharing
 ------------------------------------------------------------------------------}
--- Basic description of the functionality of an event
-data EventSeed a = EventSeed
-    { calculateE'        :: Node a -> Network (Maybe a)
-    , hasAccumE'         :: Maybe a
-    , initialArgumentsE' :: [SomeNode]
+-- Event or Behavior with observable sharing
+data Node f a = Node
+    { calculate  :: Network (f a)
+    , initialize :: Network ()
     }
 
--- Event with observable sharing
-data Event a = Event
-    { calculateE :: Network (Maybe a)
-    , initAccumE :: Network ()
-    }
-makeEvent :: EventSeed a -> Event a
-makeEvent es = unsafePerformIO $ do
-    self <- newNode
-    return $ Event
-        { calculateE = cached self (calculateE' es self)
-        , initAccumE =  }
-
--- Basic description of the functionality of a behavior
-data BehaviorSeed a = BehaviorSeed
-    { calculateB'        :: Node a -> Network a
-    , hasAccumB'         :: Maybe a
-    , initialArgumentsB' :: [SomeNode]
-    }
-
--- Behavior with observable sharing
-data Behavior a = Behavior
-    { calculateB :: Network a   -- retrieve current value
-    , initAccumB :: Network ()
-    }
-
-makeBehavior :: BehaviorSeed a -> Behavior a
-makeBehavior bs = unsafePerformIO $ do
-    self <- newNode
-    -- let calculateB = cached self (calculateB' bs self)
-    let calculateB = calculateB' bs self
-    return $ Behavior { calculateB = calculateB }
-
+type Event    = Node Maybe
+type Behavior = Node Identity
 
 -- existentially quantified version of an event or behavior
 data SomeNode = forall a. E (Event a) | forall a. B (Behavior a)
 
+-- inital description of the functionality of an event or behavior
+data NodeSeed f a = NodeSeed
+    { calculate'        :: Keys a -> Network (f a)  -- calculation function
+    , hasAccum'         :: Maybe a                  -- carries accumulator
+    , initialArguments' :: [SomeNode]               -- inital dependencies
+    }
+
+type EventSeed    = NodeSeed Maybe
+type BehaviorSeed = NodeSeed Identity
+
+-- create an event with observable sharing
+makeEvent :: EventSeed a -> Event a
+makeEvent seed = unsafePerformIO $ do
+    self <- newKeys
+    return $ Node
+        { calculate  = cached self (calculate' seed self)
+        , initialize = makeInitialize self seed }
+
+-- create a behavior with observable sharing
+makeBehavior :: BehaviorSeed a -> Behavior a
+makeBehavior seed = unsafePerformIO $ do
+    self <- newKeys
+    return $ Node
+        { calculate  = calculate' seed self
+        , initialize = makeInitialize self seed }
+
+-- functions with specialized types
+calculateE :: Event a -> Network (Maybe a)
+calculateE = calculate
+
+calculateB :: Behavior a -> Network a
+calculateB b = runIdentity <$> calculate b
 
 {-----------------------------------------------------------------------------
     Initialize a node, add it to the event graph
 ------------------------------------------------------------------------------}
+initializeSomeNode :: SomeNode -> Network ()
+initializeSomeNode (E e) = initialize e
+initializeSomeNode (B b) = initialize b
 
-initAccum :: SomeNode -> Network ()
-initAccum (E e) = initAccumE e
-initAccum (B b) = initAccumB b
-
-traverseInitAccum :: Node a -> Maybe a -> [SomeNode] -> Network ()
-traverseInitAccum self macc dependencies = do
-    b <- isAccumInitialized self
-    when (not b && isJust macc)
-        writeAccum self $! fromJust macc
-        mapM_ initAccum dependencies
+makeInitialize :: Keys a -> NodeSeed f a -> Network ()
+makeInitialize self seed = do
+    b <- readIsInit self
+    when (not b) $ do
+        -- initialize accumulator if applicable
+        case hasAccum' seed of
+            Just acc -> writeAccum self acc
+            Nothing  -> return ()
+        -- mark as initialized
+        writeIsInit self
+        -- recurse into dependencies
+        mapM_ initializeSomeNode (initialArguments' seed)
 
 {-----------------------------------------------------------------------------
     Basic combinators
@@ -196,27 +214,27 @@ traverseInitAccum self macc dependencies = do
 -- This will yield significant benefits when we collect garbage
 -- in a dynamically evolving event graph.
 never :: Event a
-never = Event { calculateE = return Nothing }
+never = Node { calculate = return Nothing, initialize = return () }
 
 mapE :: (a -> b) -> Event a -> Event b
-mapE f e = makeEvent $ EventSeed
-    { calculateE' = \_ -> fmap f <$> calculateE e
-    , hasAccumE'  = Nothing
-    , initialArgumentsE' = [E e]
+mapE f e = makeEvent $ NodeSeed
+    { calculate'        = \_ -> fmap f <$> calculateE e
+    , hasAccum'         = Nothing
+    , initialArguments' = [E e]
     }
 
 filterJust :: Event (Maybe a) -> Event a
-filterJust e = makeEvent $ EventSeed
-    { calculateE' = \_ -> return . join =<< calculateE e
-    , hasAccumE'  = Nothing
-    , initialArgumentsE' = [E e]
+filterJust e = makeEvent $ NodeSeed
+    { calculate'        = \_ -> return . join =<< calculateE e
+    , hasAccum'         = Nothing
+    , initialArguments' = [E e]
     }
 
 unionWith :: (a -> a -> a) -> Event a -> Event a -> Event a
-unionWith f e1 e2 = makeEvent $ EventSeed {
-    { calculateE' = \_ -> eval <$> calculateE e1 <*> calculateE e2
-    , hasAccumE'  = Nothing
-    , initialArgumentsE' = [E e1, E e2]
+unionWith f e1 e2 = makeEvent $ NodeSeed
+    { calculate'        = \_ -> eval <$> calculateE e1 <*> calculateE e2
+    , hasAccum'         = Nothing
+    , initialArguments' = [E e1, E e2]
     }
     where
     eval (Just x) (Just y) = Just (f x y)
@@ -226,11 +244,11 @@ unionWith f e1 e2 = makeEvent $ EventSeed {
 
 
 accumE :: a -> Event (a -> a) -> Event a
-accumE acc e = makeEvent $ EventSeed
-    { calculateE' = \self ->
+accumE acc e = makeEvent $ NodeSeed
+    { calculate'        = \self ->
             maybe (return Nothing) (accum self) =<< calculateE e
-    , hasAccumE'  = Just acc
-    , initialArgumentsE' = [E e]
+    , hasAccum'         = Just acc
+    , initialArguments' = [E e]
     }
     where
     accum self f = do
@@ -239,14 +257,14 @@ accumE acc e = makeEvent $ EventSeed
         return (Just acc')
 
 stepperB :: a -> Event a -> Behavior a
-stepperB acc e = makeBehavior $ BehaviorSeed {
-    { calculateB' = \self -> do
+stepperB acc e = makeBehavior $ NodeSeed
+    { calculate'        = \self -> do
             -- write accumulator if applicable
             maybe (return ()) (writeAccum self $!) =<< calculateE e
             -- return previous instance
-            readAccum self
-    , hasAccumB'  = Just acc
-    , initialArgumentsB' = [E e]
+            Identity <$> readAccum self
+    , hasAccum'         = Just acc
+    , initialArguments' = [E e]
     }
 
 
@@ -258,12 +276,12 @@ type Moment a = Network a
 
 -- dynamic event switching
 switchE :: Event (Moment (Event a)) -> Event a
-switchE ee = makeEvent $ EventSeed
-    { calculateE' = \self ->
+switchE ee = makeEvent $ NodeSeed
+    { calculate'        = \self ->
             -- sample current event and calculate its occurrence
             calculateE =<< calculateB bAccum
-    , hasAccumE'  = Nothing
-    , initialArgumentsE' = [E ee]
+    , hasAccum'         = Nothing
+    , initialArguments' = [E ee]
     }
     where
     -- switch into new event
@@ -271,20 +289,21 @@ switchE ee = makeEvent $ EventSeed
 
 
 observeE :: Event (Moment a) -> Event a
-observeE e = makeEvent $ EventSeed
-    { calculateE' = \self -> do
-            me <- calculateE e
-            case me of
+observeE e = makeEvent $ NodeSeed
+    { calculate'        = \self -> do
+            mx <- calculateE e
+            case mx of
                 Nothing -> return Nothing
                 Just x  -> Just <$> x
-    , hasAccumE'  = Nothing
-    , initialArgumentsE' = [E e]
+    , hasAccum'         = Nothing
+    , initialArguments' = [E e]
     }
 
 
 -- trim an event to be used with dynamic event switching
 trimE :: Event a -> Moment (Moment (Event a))
 trimE e = do
-    demand (E e)
+    initialize e
+    addDemand (E e)
     return $ return e
 
