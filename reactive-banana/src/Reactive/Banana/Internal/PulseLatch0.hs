23f74f19f5e0bc47fac0a38d5e58c00a1a450f98
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2012-07-25 12:50:35 +0200

    Rethink Latch and the handling of future values.

--- reactive-banana/src/Reactive/Banana/Internal/PulseLatch0.hs
+++ reactive-banana/src/Reactive/Banana/Internal/PulseLatch0.hs
@@ -83,18 +83,24 @@ writePulse key x =
 readPulse :: Key (Maybe a) -> Network (Maybe a)
 readPulse key = (join . Vault.lookup key . grPulse) <$> get
 
--- write latch value for later
+-- write latch value immediately
 writeLatch :: Key a -> a -> Network ()
 writeLatch key x =
-    tell $ Endo $ \g -> g { grLatch = Vault.insert key x $ grLatch g }
+    modify $ \g -> g { grLatch = Vault.insert key x $ grLatch g }
 
 -- read latch value immediately
 readLatch :: Key a -> Network a
 readLatch key = (maybe err id . Vault.lookup key . grLatch) <$> get
     where err = error "readLatch: latch not initialized!"
 
+-- write latch value for future
+writeLatchFuture :: Key a -> a -> Network ()
+writeLatchFuture key x =
+    tell $ Endo $ \g -> g { grLatch = Vault.insert key x $ grLatch g }
+
 -- read future latch value
--- warning: forcing the value early will likely result in an infinite loop
+-- Note [LatchFuture]:
+--   warning: forcing the value early will likely result in an infinite loop
 readLatchFuture :: Key a -> Network a
 readLatchFuture key = (maybe err id . Vault.lookup key . grLatch) <$> ask
     where err = error "readLatchFuture: latch not found!"
@@ -117,6 +123,11 @@ dependOns x = mapM_ $ dependOn x
         calculates the next value and makes sure that it's cached
     valueL/P
         retrieves the current value
+    futureL
+        future value of the latch
+        see note [LatchFuture]
+    uidL/P
+        used for dependency tracking and evaluation order
 -}
 
 data Pulse a = Pulse
@@ -128,8 +139,27 @@ data Pulse a = Pulse
 data Latch a = Latch
     { evaluateL :: Network ()
     , valueL    :: Network a
+    , futureL   :: Network a
     , uidL      :: Unique
     }
+
+{- Note [LatchCreation]
+
+When creating a new latch from a pulse, we assume that the
+pulse cannot fire at the moment that the latch is created.
+This is important when switching latches, because of note [PulseCreation].
+
+Likewise, when creating a latch, we assume that we do not
+have to calculate the previous latch value.
+
+Note [PulseCreation]
+
+We assume that we do not have to calculate a pulse occurrence
+at the moment we create the pulse. Otherwise, we would have
+to recalculate the dependencies *while* doing evaluation;
+this is a recipe for desaster.
+
+-}
 
 -- make pulse from evaluation function
 pulse :: Network (Maybe a) -> Network (Pulse a)
@@ -156,13 +186,32 @@ latch :: a -> Network (Maybe a) -> Network (Latch a)
 latch a eval = do
     key <- liftIO Vault.newKey
     uid <- liftIO newUnique
-    writeLatch key a    -- initialize latch value
+
+    -- Initialize with future latch value.
+    -- See note [LatchCreation].
+    writeLatchFuture key a
+
     return $ Latch
-        { evaluateL = maybe (return ()) (writeLatch key) =<< eval
+        { evaluateL = maybe (return ()) (writeLatchFuture key) =<< eval
         , valueL    = readLatch key
+        , futureL   = readLatchFuture key
         , uidL      = uid
         }
 
+pureL :: a -> Network (Latch a)
+pureL a = do
+    uid <- liftIO newUnique
+    return $ Latch
+        { evaluateL = return ()
+        , valueL    = return a
+        , futureL   = return a
+        , uidL      = uid
+        }
+
+{-----------------------------------------------------------------------------
+    Existential quantification over Pulse and Latch
+    for dependency tracking
+------------------------------------------------------------------------------}
 data SomeNode = forall a. P (Pulse a) | forall a. L (Latch a)
 
 instance Eq SomeNode where
@@ -179,23 +228,40 @@ instance Hashable SomeNode where
 ------------------------------------------------------------------------------}
 stepperL :: a -> Pulse a -> Network (Latch a)
 stepperL a p = do
+    -- @a@ is indeed the future latch value. See note [LatchCreation].
     x <- latch a (valueP p)
-    (L x) `dependOn` (P p)
+    L x `dependOn` P p
     return x
 
 accumP :: a -> Pulse (a -> a) -> Network (Pulse a)
 accumP a p = mdo
         x       <- stepperL a result
         result  <- pulse $ eval <$> valueL x <*> valueP p
-        P result `dependOns` [L x, P p]
+        -- Evaluation order of the result pulse does *not*
+        -- depend on the latch. It does depend on latch value,
+        -- though, so don't garbage collect that one.
+        P result `dependOn` P p
         return result
     where
     eval a Nothing  = Nothing
-    eval a (Just f) = Just (f a)
+    eval a (Just f) = let b = f a in b `seq` Just b  -- strict evaluation
+
+applyP :: Latch (a -> b) -> Pulse a -> Network (Pulse b)
+applyP f x = do
+    result <- pulse $ fmap <$> valueL f <*> valueP x
+    P result `dependOn` P x
+    return result
+
 
 mapP :: (a -> b) -> Pulse a -> Network (Pulse b)
 mapP f p = do
     result <- pulse $ fmap f <$> valueP p
+    P result `dependOn` P p
+    return result
+
+filterJustP :: Pulse (Maybe a) -> Network (Pulse a)
+filterJustP p = do
+    result <- pulse $ join <$> valueP p
     P result `dependOn` P p
     return result
 
@@ -209,6 +275,17 @@ unionWith f px py = do
     eval (Just x) Nothing  = Just x
     eval Nothing  (Just y) = Just y
     eval Nothing  Nothing  = Nothing
+
+
+applyL :: Latch (a -> b) -> Latch a -> Network (Latch b)
+applyL lf lx = do
+    -- The value in the next cycle is always the future value.
+    -- See note [LatchCreation].
+    let eval = ($) <$> futureL lf <*> futureL lx
+    a <- eval
+    result <- latch a $ fmap Just eval
+    L result `dependOns` [L lf, L lx]
+    return result
 
 {-----------------------------------------------------------------------------
     Combinators - dynamic event switching
@@ -234,10 +311,30 @@ switchP pp = mdo
                 Nothing -> return ()
                 Just p  -> P result `dependOn` P p  -- check in new pulse
             valueP =<< valueL lp                    -- fetch value from old pulse
+            -- we have to use the *old* event value due to note [LatchCreation]
     result <- pulse eval
     P result `dependOns` [L lp, P pp]
     return result
 
 
+switchL :: Latch a -> Pulse (Latch a) -> Network (Latch a)
+switchL l p = mdo
+    ll <- stepperL l p
+    let
+        -- switch to a new latch
+        switchTo l = do
+            L result `dependOn` L l
+            futureL l
+        -- calculate future value of the result latch
+        eval = do
+            mp <- valueP p
+            case mp of
+                Nothing -> futureL =<< valueL ll
+                Just l  -> switchTo l
+
+    a      <- futureL l                 -- see note [LatchCreation]
+    result <- latch a $ Just <$> eval
+    L result `dependOns` [L l, P p]
+    return result
 
 
