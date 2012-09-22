7830eaf974ca99dc6e02d15225adcbfc1a75e8fa
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2012-09-22 16:30:42 +0200

    Reactive-banana now compiles with the haste compiler.
    haste is a Haskell -> JavaScript compiler that uses the GHC front-end and parts of the back-end.
    
      [haste]: https://github.com/valderman/haste-compiler/

---
+++ reactive-banana/src/JS/Data/HashMap/Lazy.hs
@@ -0,0 +1,21 @@
+-- dummy implementation for the haste compiler
+module JS.Data.HashMap.Lazy where
+
+import JS.Data.Hashable
+
+import qualified Data.List as List
+import qualified Data.IntMap as Map
+
+newtype HashMap k v = M { unM :: Map.IntMap [(k,v)] } deriving (Show)
+
+empty :: HashMap k v
+empty = M Map.empty
+
+lookup :: (Eq k, Hashable k) => k -> HashMap k v -> Maybe v
+lookup k m = List.lookup k =<< Map.lookup (hash k) (unM m)
+
+insertWith :: (Eq k, Hashable k) =>
+    (v -> v -> v) -> k -> v -> HashMap k v -> HashMap k v
+insertWith f k new = M . Map.insertWith helper (hash k) [(k,new)] . unM
+    where
+    helper list _ = [(k,f new old) | (k1,old) <- list, k1 == k]
---
+++ reactive-banana/src/JS/Data/Hashable.hs
@@ -0,0 +1,25 @@
+-- dummy implementation for the haste compiler
+module JS.Data.Hashable where
+
+import Data.Bits
+import Data.Unique.Really
+
+class Hashable a where
+    hash :: a -> Int
+    hash = hashWithSalt defaultSalt
+    
+    hashWithSalt :: Int -> a -> Int
+    hashWithSalt salt x = salt `combine` hash x
+
+defaultSalt :: Int
+defaultSalt = 2166136261
+
+instance Hashable Unique where
+    hashWithSalt s u = s `combine` hashUnique u
+
+instance Hashable Char where hash = fromEnum
+
+-- | Combine two given hash values.  'combine' has zero as a left
+-- identity.
+combine :: Int -> Int -> Int
+combine h1 h2 = (h1 * 16777619) `xor` h2
