a372635dc044773085375a9a47af3c44466cf67b
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2013-03-15 17:07:13 +0100

    Add test for leak reported in issue #25

--- reactive-banana/src/Reactive/Banana/Test/SpaceLeaks.hs
+++ reactive-banana/src/Reactive/Banana/Test/SpaceLeaks.hs
@@ -3,13 +3,14 @@
 
     Testing for particular space use
 ------------------------------------------------------------------------------}
+{-# LANGUAGE ScopedTypeVariables #-}
 import Control.Exception (evaluate)
 import Control.Monad (forM_)
 
 import Reactive.Banana
 import Reactive.Banana.Frameworks
 
-main = test_stepper_strict
+main = test_behavior_apply
 
 {-----------------------------------------------------------------------------
     Space Leaks
@@ -42,5 +43,27 @@ test_stepper_strict = do
 
     forM_ [1..8000] $ \n -> fire ()
 
+-- reported by  bjoeris
+test_behavior_apply :: IO ()
+test_behavior_apply = do
+    (handler, fire) <- newAddHandler
+    let
+        networkDescription :: forall t. (Frameworks t) => Moment t ()
+        networkDescription = do
+            foo <- fromAddHandler handler
+            -- let bar = (pure (const True) :: Behavior t (() -> Bool)) `apply` foo
+            
+            -- this line has the same problem:
+            let bar = (pure (const True) :: Behavior t (()->Bool)) `apply` never :: Event t Bool 
 
+            -- this line doesn't
+            -- let bar = (const True) `fmap` foo                              
+            
+            reactimate $ fmap (const $ return ()) foo
+            
+            -- commenting out this line fixes the problem
+            reactimate $ fmap (const $ return ()) bar
 
+    network <- compile networkDescription
+    actuate network
+    forM_ [1..3*10^4] $ \n -> fire ()
