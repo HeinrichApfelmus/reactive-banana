4cd3c8864c15b88b21d31cb37bc8ee23c5a71198
Mathieu Suen <mathk.sue@gmail.com>
2014-11-19 15:55:13 +0100

    Stuffing more intresting example in Matrix

--- reactive-banana-gtk/src/Matrix.hs
+++ reactive-banana-gtk/src/Matrix.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-
 import          Graphics.UI.Gtk
 import          Data.Vector
 import          Reactive.Banana
@@ -11,10 +9,8 @@ validateInput :: Maybe Integer -> String
 validateInput Nothing = "Invalid input"
 validateInput (Just int) = show int
 
-main :: IO ()
-main = do
-    initGUI
-    window <- windowNew
+buildRow :: IO (HBox, Entry, Label)
+buildRow = do
     entry <- entryNew
     label <- labelNew (Just "Enter text")
 
@@ -22,20 +18,52 @@ main = do
     hbox <- hBoxNew True 0
     containerAdd hbox entry
     containerAdd hbox label
+    return (hbox, entry, label)
 
-    set window [ containerBorderWidth := 10, containerChild := hbox]
+buttonEvent :: (Frameworks t, ButtonClass b) => b ->  Moment t (Event t ())
+buttonEvent = flip event0 $ buttonActivated
+
+
+networkAddingInput :: (ContainerClass w, ButtonClass b, Frameworks t) => w -> b ->  Moment t ()
+networkAddingInput widget button = do
+    eButton <- buttonEvent button 
+    let newNetwork :: Frameworks t1 => Moment t1 ()
+        newNetwork = do
+            (w, entry, label) <- liftIO buildRow
+            liftIO $ containerAdd widget w
+            liftIO $ widgetShowAll w
+            networkForInput entry label
+
+    execute $ (FrameworksMoment newNetwork <$ eButton)
+    return ()
+
+networkForInput :: (EntryClass e, EditableClass e, LabelClass l, Frameworks t) => e -> l -> Moment t ()
+networkForInput entry label = do
+    eEntry <- eventEntry entry
+    let bEntry = stepper "" eEntry
+        bMaybe = readMaybe <$> bEntry
+    sink label [labelLabel :== validateInput <$> bMaybe]
+
+main :: IO ()
+main = do
+    initGUI
+    window <- windowNew
+    button <- buttonNew
+    (initBox, entry, label) <- buildRow
+
+    -- Layout
+    vbox <- vBoxNew True 0
+    containerAdd vbox button
+    containerAdd vbox initBox
+
+    -- Setting attribute
+    set button [buttonLabel := "Add More Input"]
+    set window [ containerBorderWidth := 10, containerChild := vbox]
     
     -- Network
-    let networkDescription :: forall t. Frameworks t => Moment t ()
-        networkDescription  = do
-            eEntry <- eventEntry entry
-            let bEntry = stepper "" eEntry
-                bMaybe = readMaybe <$> bEntry
-            sink label [labelLabel :== validateInput <$> bMaybe]
-
-    network <- compile networkDescription
+    network <- compile $ networkForInput entry label >> networkAddingInput vbox button
     actuate network
+    
     onDestroy window mainQuit
     widgetShowAll window
-
     mainGUI
