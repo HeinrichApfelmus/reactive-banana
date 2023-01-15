259cb5a65139da58a9f1f53d157738d106631f9c
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2023-01-15 13:49:39 +0100

    Split `runNetworkSizes`

--- reactive-banana/tests/space.hs
+++ reactive-banana/tests/space.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE BangPatterns #-}
+{-----------------------------------------------------------------------------
+    reactive-banana
+------------------------------------------------------------------------------}
 module Main where
 
 import Control.Monad
-  ( void )
+  ( foldM, void )
 
 import qualified Reactive.Banana.Test.Mid.Space as Mid
 import qualified Reactive.Banana.Test.High.Space as High
@@ -11,8 +15,19 @@ main = do
     say "Running..."
     -- void $ High.runNetworkSizes High.executeAccumE1 [1..30000]
     -- void $ High.runNetworkSizes High.observeAccumE1 [1..10000]
-    void $ Mid.runNetworkSizes Mid.executeAccum1 [1..50000]
+    void $ runMidNetwork Mid.executeAccum1 [1..50000]
     say "Done"
 
 say :: String -> IO ()
 say = putStrLn
+
+{-----------------------------------------------------------------------------
+    Test harness
+------------------------------------------------------------------------------}
+runMidNetwork f xs = do
+    (network0, step) <- Mid.compileToStateMachine f
+    void $ runStrict step xs network0
+
+runStrict :: Monad m => (a -> s -> m s) -> [a] -> s -> m s
+runStrict f [] !s = pure s
+runStrict f (x:xs) !s = runStrict f xs =<< f x s
